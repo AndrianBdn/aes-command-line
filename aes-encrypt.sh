e9ca18c83548179bb4212dd9728463a0284576ee
Andrian Budantsov <andrian@readdle.com>
2019-06-30 11:06:32 +0300

    Change: added aes-view, replaced srm to rm (not useful on SSD anyway)

--- aes-encrypt.sh
+++ aes-encrypt.sh
@@ -3,11 +3,20 @@
 CIPHER="aes-256-cbc"
 SCRIPT=`basename $0`
 SCRIPT=${SCRIPT%.sh}
-SRM_CMD=srm
+SRM_CMD=rm
 
 command -v openssl >/dev/null 2>&1 || { echo "Could not find openssl in path"; exit 1; }
 command -v $SRM_CMD >/dev/null 2>&1 || SRM=shred;
 command -v $SRM_CMD >/dev/null 2>&1 || { echo "Could not find 'srm' or 'shred' for secure deletion"; exit 1; }
+
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "** Trapped CTRL-C"
+        clear # clear screen 
+        printf '\e]50;ClearScrollback\a' # clear iTerm scroll back https://superuser.com/questions/576410/how-can-i-partially-clear-my-terminal-scrollback
+        exit 1
+}
 
 
 if [ $# -ne 1 ]
@@ -20,38 +29,54 @@ if test -r "$1" -a -f "$1"
 then
   cd `dirname $1`
 
-  if [ "$SCRIPT" = "aes-encrypt" ]
-  then
+  case "$SCRIPT" in 
 
-    openssl $CIPHER -salt -in $1 -out $1.aes
-    if [ $? -eq 0 ]
-    then
-      echo "- created $1.aes in $(pwd)"
-      echo "- securely deleting using $SRM_CMD"
-      $SRM_CMD $1
-      echo "- original file $1 deleted"
-    else
-      echo "- encryption failed, file is still there"
-    fi
+    "aes-encrypt")
+      openssl $CIPHER -salt -in $1 -out $1.aes
+      if [ $? -eq 0 ]
+      then
+        echo "- created $1.aes in $(pwd)"
+        echo "- securely deleting using $SRM_CMD"
+        $SRM_CMD $1
+        echo "- original file $1 deleted"
+      else
+        echo "- encryption failed, file is still there"
+      fi
+    ;;
 
-  else
+    "aes-decrypt")
 
-    out=${1%.aes}
-    if [ "$1" != "$out.aes" ]
-    then
-        echo "- $SCRIPT argument should contain .aes extension"
-        exit 1
-    fi
-      
-    openssl $CIPHER -salt -d -in $1 -out $out
-    if [ $? -eq 0 ]
-    then
-      echo "- decrypted $out in $(pwd)"
-      rm $1
-      echo "- deleted $1"
-    fi
+      out=${1%.aes}
+      if [ "$1" != "$out.aes" ]
+      then
+          echo "- $SCRIPT argument should contain .aes extension"
+          exit 1
+      fi
+        
+      openssl $CIPHER -salt -d -in $1 -out $out
+      if [ $? -eq 0 ]
+      then
+        echo "- decrypted $out in $(pwd)"
+        rm $1
+        echo "- deleted $1"
+      fi
+    ;;
 
-  fi
+    "aes-view")
+
+      out=${1%.aes}
+      if [ "$1" != "$out.aes" ]
+      then
+          echo "- $SCRIPT argument should contain .aes extension"
+          exit 1
+      fi
+
+      openssl $CIPHER -salt -d -in $1 
+      sleep 7200
+
+    ;;
+
+  esac
 
 
 else
--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ echo "Installing to $DESTDIR..."
 chmod 755 aes-encrypt.sh
 cp -f aes-encrypt.sh $DESTDIR/bin/aes-encrypt
 ln -sf $DESTDIR/bin/aes-encrypt $DESTDIR/bin/aes-decrypt
+ln -sf $DESTDIR/bin/aes-encrypt $DESTDIR/bin/aes-view
+
+echo "done: you may use aes-encrypt, aes-decrypt, aes-view now"
